9da5590b3704926e36a971c61d34623ca11bbbe9
João Pedro Magrini <reccojp65@gmail.com>
2023-05-26 10:50:07 -0300

    ....

--- JS/script.js
+++ JS/script.js
@@ -1,13 +1,13 @@
 // variaveis
-let bnt1 = 0;
-let bnt2 = 0;
-let bnt3 = 0;
-let bnt4 = 0;
-let bnt5 = 0;
-let bnt6 = 0;
-let bnt7 = 0;
-let bnt8 = 0;
-let bnt9 = 0;
+let btn1 = 0;
+let btn2 = 0;
+let btn3 = 0;
+let btn4 = 0;
+let btn5 = 0;
+let btn6 = 0;
+let btn7 = 0;
+let btn8 = 0;
+let btn9 = 0;
 let controle = 0;
 let jogadas = 0;
 let finaljogo = false;
@@ -20,11 +20,166 @@ function clickbtn1(){
         if (controle == 0) {
             controle = 1;
             btn.value = "X";
-            bnt1 = "X";
+            btn1 = "X";
         } else {
             controle = 0;
             btn.value = "O";
             btn1 = "O";
         }
     }
+    verificaganhador()
+}
+
+// click botão 2
+function clickbtn2(){
+    let btn = document.getElementById('btn2');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn2 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn2 = "O";
+        }
+    }
+    verificaganhador()
+}
+
+// click botão 3
+function clickbtn3(){
+    let btn = document.getElementById('btn3');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn3 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            bnt3 = "O";
+        }
+    }
+    verificaganhador()
+}
+
+// click botão 4
+function clickbtn4(){
+    let btn = document.getElementById('btn4');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn4 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn4 = "O";
+        }
+    }
+    verificaganhador()
+}
+
+// click botão 5
+function clickbtn5(){
+    let btn = document.getElementById('btn5');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn5 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn5 = "O";
+        }
+    }
+    verificaganhador()
+}
+// click botão 6
+function clickbtn6(){
+    let btn = document.getElementById('btn6');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn6 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn6 = "O";
+        }
+    }
+    verificaganhador()
+}
+// click botão 7
+function clickbtn7(){
+    let btn = document.getElementById('btn7');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn7 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            bnt7 = "O";
+        }
+    }
+    verificaganhador()
+}
+// click botão 8
+function clickbtn8(){
+    let btn = document.getElementById('btn8');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn8 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            bnt8 = "O";
+        }
+    }
+    verificaganhador()
+}
+
+// click botão 9
+function clickbtn9(){
+    let btn = document.getElementById('btn9');
+
+    if (btn.value == ' ' && !finaljogo) {
+        if (controle == 0) {
+            controle = 1;
+            btn.value = "X";
+            btn9 = "X";
+        } else {
+            controle = 0;
+            btn.value = "O";
+            btn9 = "O";
+        }
+    }
+    verificaganhador()
+}
+
+// Verifica Ganhador
+function verificaganhador() {
+    let lbljogador = document.getElementById("lbljogador")
+    let lbljogadas = document.getElementById("lbljogadas")
+
+// Verifica se jogador X ganhou     
+    if(btn1=='X' && btn2=='X' && btn3=='X') {
+        finaljogo = true;
+        alert('O Jogador X ganhou')
+    }
+
 }
