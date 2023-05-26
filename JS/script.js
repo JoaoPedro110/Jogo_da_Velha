113a2a6a65f3300664446d961b57caadcc8424b0
João Pedro Magrini <reccojp65@gmail.com>
2023-05-26 11:34:48 -0300

    ;............

--- JS/script.js
+++ JS/script.js
@@ -178,38 +178,54 @@ function verificaganhador()
     let lbljogadas = document.getElementById("lbljogadas")
 
 // Verifica se jogador X ganhou    
- 
+
     if(btn1=='X' && btn2=='X' && btn3=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
     if(btn4=='X' && btn5=='X' && btn6=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
     if(btn7=='X' && btn8=='X' && btn9=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
     if(btn1=='X' && btn4=='X' && btn7=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
     if(btn2=='X' && btn5=='X' && btn8=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
     if(btn3=='X' && btn6=='X' && btn9=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
     if(btn1=='X' && btn5=='X' && btn9=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
     if(btn3=='X' && btn5=='X' && btn7=='X') {
         finaljogo = true;
-        alert('O Jogador X ganhou')
+        lbljogador.innerText = 'Jogador X ganhou!';
+        lbljogadas.innerText = '';
+
     }
 
 
@@ -217,40 +233,40 @@ function verificaganhador()
 
     if(btn1=='O' && btn2=='O' && btn3=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+
     }
 
     if(btn4=='O' && btn5=='O' && btn6=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+
     }
 
     if(btn7=='O' && btn8=='O' && btn9=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+
     }
 
     if(btn1=='O' && btn4=='O' && btn7=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+
     }
 
     if(btn2=='O' && btn5=='O' && btn8=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+    
     }
 
     if(btn3=='O' && btn6=='O' && btn9=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+   
     }
     if(btn1=='O' && btn5=='O' && btn9=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+    
     }
 
     if(btn3=='O' && btn5=='O' && btn7=='O') {
         finaljogo = true;
-        alert('O Jogador O ganhou')
+    
     }
 }
